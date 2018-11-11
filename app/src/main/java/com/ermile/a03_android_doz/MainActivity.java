86b5e55f98289f3113e732eab91ad9cb29d3c5a9
amingoli78 <amin.foli78@gmail.com>
2018-11-11 19:14:05 +0330

    first push

---
+++ app/src/main/java/com/ermile/a03_android_doz/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ermile.a03_android_doz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
