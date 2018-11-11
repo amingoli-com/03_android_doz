987f4b66b78b3b3b888757d58c9d078f1becd068
amingoli78 <amin.foli78@gmail.com>
2018-11-11 19:52:50 +0330

    set on click by error

--- app/src/main/java/com/ermile/a03_android_doz/MainActivity.java
+++ app/src/main/java/com/ermile/a03_android_doz/MainActivity.java
@@ -2,6 +2,8 @@ package com.ermile.a03_android_doz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onclick(View view){
+        ImageView img = (ImageView) view;
+        img.setImageResource(R.drawable.pone);
     }
 }
