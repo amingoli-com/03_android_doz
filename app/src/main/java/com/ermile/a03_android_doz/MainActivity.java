c23e2e94a3340e21c6180d50e75b65018143d069
amingoli78 <amin.foli78@gmail.com>
2018-11-12 11:22:31 +0330

    dbug & lanch game by xml.

--- app/src/main/java/com/ermile/a03_android_doz/MainActivity.java
+++ app/src/main/java/com/ermile/a03_android_doz/MainActivity.java
@@ -1,9 +1,10 @@
 package com.ermile.a03_android_doz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
-    public void onclick(View view){
-        ImageView img = (ImageView) view;
-        img.setImageResource(R.drawable.pone);
+    public void oc_start_game (View view){
+        startActivity ( new Intent(MainActivity.this,activity_game.class));
     }
 }
